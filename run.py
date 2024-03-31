6da2bf58a6c2168094ff8763d8727c46214ab81c
ayub <ayubhaidari895@gmail.com>
2024-03-31 16:21:17 +0800

    my firs commit

---
+++ run.py
@@ -0,0 +1,18 @@
+# First_name = input('Enter your first name : ')  #input('')  >>> NO SPACE()
+# Last_name = input('Enter your Last name : ') # for string , NEED TO '' 
+# age = input('How old are you? ')
+#age = 25 #>>>>> # For numbers No need ''.
+
+
+# sentence = 'you are {}'.format(First_name)  #'string {}'.format(variable) 
+
+#  sentence = f'you are {First_name} {Last_name}. Your age is {age}'    #>>>> NEW: f'string {variable}'
+
+# print (sentence)
+
+# a = 'a'
+# b = 4
+# For knowing the type: type(variable)
+# print (type(a))
+# print (type(b))
+# hi
